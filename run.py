d2322da34b73f3313c8463d4e30d34d105c8c7cd
F-Clement <>
2023-06-16 19:44:26 +0000

    add function validate sales count to check that sales are integers and sales is not greater than clicks

--- run.py
+++ run.py
@@ -22,22 +22,34 @@ print("Enter the name of the product you sell. Example Phones, courses, cars etc
 product = input("Produt:")
 
 print(f"Great! How much have you been ivesting to advertise {product} on Facebook, Youtube,Instagram and Tiktok?")
-budget = input("Budget:")
+budget = input("Budget: $")
 
 investment_per_platform = float(budget)/4
 clicks = int(investment_per_platform / 2)
 print(f"So you have been investing {investment_per_platform}€ on each advertising platform. Thus approximately {clicks} clicks per week.\n")
 
 def get_average_sales():
+    """
+    Get number of sales made on each platform
+    """
     print(f"What is the average number of sales you make on each of the platforms?")
     facebook = input("Facebook:")
     youtube = input("Youtube:")
     instagram = input("Instagram:")
     tiktok = input("TikTok:")
+    average_sales = [facebook, youtube, instagram, tiktok]
+    
+    validate_sales_count(average_sales)
 
-    average_sales = [int(facebook), int(youtube), int(instagram), int(tiktok)]
-
-    print(f"Average sales = {average_sales}\n")
-    return average_sales
+def validate_sales_count(sales_data):
+    try:
+        [int(sale) for sale in sales_data]
+        for sale in sales_data:
+            if int(sale) > clicks:
+                raise ValueError(
+                    f"The input '{sale}' for sales is not correct. Number of Sales cannot be more than Number of clicks. \n"
+                )
+    except ValueError as e:
+        print(f"Invalid data: {e} Please enter Integer values for sales made")
 
 get_average_sales()
