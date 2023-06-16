92716fa785467a35801594c4b56858e8809da486
F-Clement <>
2023-06-16 18:15:13 +0000

    create get average sales function so that user can input weekly averages sales of product on each platform

--- run.py
+++ run.py
@@ -26,4 +26,18 @@ budget = input("Budget:")
 
 investment_per_platform = float(budget)/4
 clicks = int(investment_per_platform / 2)
-print(f"So you have been investing {investment_per_platform}€ on each advertising platform. Thus approximately {clicks} clicks per week.")
+print(f"So you have been investing {investment_per_platform}€ on each advertising platform. Thus approximately {clicks} clicks per week.\n")
+
+def get_average_sales():
+    print(f"What is the average number of sales you make on each of the platforms?")
+    facebook = input("Facebook:")
+    youtube = input("Youtube:")
+    instagram = input("Instagram:")
+    tiktok = input("TikTok:")
+
+    average_sales = [int(facebook), int(youtube), int(instagram), int(tiktok)]
+
+    print(f"Average sales = {average_sales}\n")
+    return average_sales
+
+get_average_sales()
