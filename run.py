9d7f49d13280ecb599cf9605dd458a0589c2bd8d
F-Clement <>
2023-06-17 11:06:01 +0000

    update maths strategy used to calculate investment on market strategy function

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('product_marketers')
 
 
-print("_____----------Welcome To Product Marketers----------_____ \n")
+print("_____------Welcome To Product Marketers------_____ \n")
 print("Enter the name of the product you sell.")
 print("Example Phones, courses, cars etc.")
 product = input("Produt:")
@@ -31,8 +31,8 @@ while True:
         print(f"The Budget must be an interger.")
 investment_per_platform = float(budget)/4
 clicks = int(investment_per_platform / 2)
-print(f"We assume you have been investing {investment_per_platform}€ ")
-print("on each advertising platform. About {clicks} clicks / week.\n")
+print(f"We assume you have been investing ${investment_per_platform} ")
+print(f"on each advertising platform. About {clicks} clicks / week.\n")
 
 def get_average_sales():
     """
@@ -46,7 +46,7 @@ def get_average_sales():
         instagram = input("Instagram:")
         tiktok = input("TikTok:")
         average_sales = [facebook, youtube, instagram, tiktok]
-        if(validate_sales_count(average_sales)):
+        if (validate_sales_count(average_sales)):
             print("Thank you! Building a marketing strategy now...")
             return average_sales
 
@@ -66,19 +66,22 @@ def validate_sales_count(sales_data):
 
 def update_average_sales_worksheet(sales_data):
     """
-    Update the averages sales work sheet with the sales values the user provided
+    Update the averages sales work sheet
+    with the sales values the user provided
     """
-    print(f"Adding sales values you provided to average sales work sheet......")
+    print(f"Adding sales values you provided to average sales work sheet..")
     average_sales_sheet = SHEET.worksheet('average_sales')
     average_sales_sheet.append_row(sales_data)
-    print(f"Average sales values uploaded successfully to average sales worksheet.")
+    print(f"Average sales values uploaded to worksheet successfully.")
 
 def update_diff_btw_clicks_and_sales(data, click):
     """
     Update our diff_btw_clics_sales worksheet
-    Data to update comes from subtracting number of sales from number of clicks for each platform
+    Data to update comes from subtracting number
+    of sales from number of clicks for each platform
     """
-    print(f"We calculate the difference between click and sales and add to diff_btw_clicks_sales worksheet")
+    print(f"We calculate the difference between clicks")
+    print("and sales and add to diff_btw_clicks_sales worksheet")
     diff_btw_clicks_sales_sheet = SHEET.worksheet('diff_btw_clicks_and_sales')
     diff = [clicks - int(value) for value in data]
     diff_btw_clicks_sales_sheet.append_row(diff)
@@ -95,18 +98,20 @@ def market_strategy(diff, clicks, investment):
     print(f"Calculating a better strategy to invest ${budget}....")
     strategised_investment = []
     for value in diff:
-        if value >= clicks * 0.8:
-            sum_to_invest = investment/3
+        if value < clicks * 0.2:
+            sum_to_invest = int(investment * 2)
             strategised_investment.append(sum_to_invest)
-        elif value >= clicks * 0.5:
-            sum_to_invest = investment/2
+        elif value < clicks * 0.5:
+            sum_to_invest = int(investment * 1.5)
             strategised_investment.append(sum_to_invest)
-        elif value >= clicks * 0.3:
-            sum_to_invest = investment
+        elif value < clicks * 0.7:
+            sum_to_invest = int(investment/2)
+            strategised_investment.append(sum_to_invest)
+        elif value < clicks * 0.9:
+            sum_to_invest = int(investment/3)
             strategised_investment.append(sum_to_invest)
         else:
-            value >= clicks * 0.1
-            sum_to_invest = investment * 2
+            sum_to_invest = 0
             strategised_investment.append(sum_to_invest)
     return strategised_investment
 
@@ -119,16 +124,17 @@ def update_investment_strategy_worksheet(data, product):
     investment_strategy_sheet.append_row(data)
     return data
 
-            
-
-
 sales = get_average_sales()
 average_sales = [int(sale) for sale in sales]
-print(f"Sale values provided for the platforms facebook, youtube, Instagram and TikTok respectively\n{average_sales}\n")
+print(f"Sale values provided for the platforms")
+print(f"facebook, youtube, Instagram and TikTok respectively")
+print(f"{average_sales}\n")
 update_average_sales_worksheet(average_sales)
 diff_btw_sales_clicks = update_diff_btw_clicks_and_sales(average_sales, clicks)
 new_invest = market_strategy(diff_btw_sales_clicks, clicks, investment_per_platform )
-print(new_invest)
+print(clicks)
+print(investment_per_platform)
 investment_per_product = update_investment_strategy_worksheet(new_invest, product)
+print(f"To make more {product} sales, Ivest as instructed below")
+print("for Facebook, Youtube, Instagram and TikTok")
 print(investment_per_product)
-print(product)
